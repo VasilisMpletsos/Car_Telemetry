600e21fa66b1f3cfe76430675625b985be8ef0c0
Vasilis Mpletsos <58275997+VasilisMpletsos@users.noreply.github.com>
2020-08-02 22:53:10 +0300

    Now with a simple post it will emit to all listeners!

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const bodyParser = require('body-parser')
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+// parse application/json
+app.use(bodyParser.json());
+
 server.listen(3000,()=>{
   console.log('Server is listening on port 3000!');
 });
-
-io.on('connection',(socket)=>{
-  socket.emit('welcome');
-  socket.on('steeringData',(rotation)=>{
-    io.emit('steeringUpdate',rotation)
-  })
-})
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/',(req,res)=>{
   res.redirect('html/index.html');
+})
+
+app.post('/update',(req,res)=>{
+  let rotation = req.body.rotation;
+  io.emit('steeringUpdate',rotation);
+  res.send(
+    {success: 'Sucess',
+      info: 'Data were sent'
+    })
 })
